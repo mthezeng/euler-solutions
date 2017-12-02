dcd0bb3502e1d9a0d0b8380b36e051a06a4f1ad6
teeveeJS <teemu.eemeli99@gmail.com>
2017-12-02 00:48:24 -0800

    timer doc

--- timer.py
+++ timer.py
@@ -1,7 +1,15 @@
 import time
-# import progressbar
 
 def timer(f, args=[]):
+    """
+    Calculate the runtime of a function
+    params:
+        f: callable. function whose runtime is to be measured
+        args: list. optional parameters to be passed into f
+    returns:
+        end_time: float. the runtime of f
+    NOTE: currently it is better if f itself prints its output
+    """
     start_time = time.time()
     f(*args)
     end_time = time.time() - start_time
