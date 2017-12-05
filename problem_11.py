f9d0d3f2966d4b3905c8ad5b847af4c130a9da78
mzeng7 <mzeng7@berkeley.edu>
2017-12-05 14:36:29 -0800

    finish left and right diagonals

--- problem_11.py
+++ problem_11.py
@@ -26,27 +26,46 @@ def build_columns(grid):
     grid.seek(0)
     return columns
 
-def build_leftdiagonal(grid):
-    #starting from the top left corner and going towards the bottom right
-    current_row, current_column = 1, 1
-    leftdiagonals = []
+def build_diagonal(grid, start):
+    """if start is 'left', then start from the top left corner
+    and go towards the bottom right. if start is 'right', then
+    start from the top right corner and go towards to bottom left."""
+    if start == 'left':
+        current_row, current_column = 1, 1
+    elif start == 'right':
+        current_row, current_column = 1, 20
+    diagonals = []
     for c in range(20):
+        if start == 'left':
+            current_row, current_column = 1, c + 1
+        elif start == 'right':
+            current_row, current_column = 1, 20 - c
+        end = False
         current_diagonal = []
-        current_row, current_column = 1, c + 1
-        while current_column >= 1:
+        while not end:
             grid.seek((60 * (current_row - 1)) + (3 * (current_column - 1)))
             current_diagonal.append(int(grid.read(2)))
             print(current_diagonal)
-            current_column -= 1
-            current_row += 1
-        leftdiagonals.append(deepcopy(current_diagonal))
+            if start == 'left':
+                current_column -= 1
+                current_row += 1
+                if current_column < 1:
+                    end = True
+            elif start == 'right':
+                current_column += 1
+                current_row += 1
+                if current_column > 20:
+                    end = True
+        diagonals.append(deepcopy(current_diagonal))
         print('Appended.')
-    return leftdiagonals
+    return diagonals
 
 twentygrid = open('twentygrid.txt')
-#row_list = build_rows(twentygrid)
+row_list = build_rows(twentygrid)
 #print(row_list)
-#column_list = build_columns(twentygrid)
+column_list = build_columns(twentygrid)
 #print(column_list)
-leftdiagonal_list = build_leftdiagonal(twentygrid)
+leftdiagonal_list = build_diagonal(twentygrid, 'left')
 print(leftdiagonal_list)
+rightdiagonal_list = build_diagonal(twentygrid, 'right')
+print(rightdiagonal_list)
