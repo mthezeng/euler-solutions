14609202a081bc4faa5f3d5daf12393259104cb2
mzeng7 <mzeng7@berkeley.edu>
2017-12-05 14:10:01 -0800

    starting on diagonal, still incomplete

--- problem_11.py
+++ problem_11.py
@@ -23,11 +23,30 @@ def build_columns(grid):
             grid.seek((60*(r+1))+(3*c))
         columns.append(deepcopy(current_column))
         grid.seek(3*c)
+    grid.seek(0)
     return columns
 
+def build_leftdiagonal(grid):
+    #starting from the top left corner and going towards the bottom right
+    current_row, current_column = 1, 1
+    leftdiagonals = []
+    current_diagonal = []
+    for c in range(4):
+        current_row, current_column = 1, c + 1
+        while current_column >= 1:
+            grid.seek((60 * (current_row - 1)) + (3 * (current_column - 1)))
+            current_diagonal.append(int(grid.read(2)))
+            print(current_diagonal)
+            current_column -= 1
+            current_row += 1
+        leftdiagonals.append(deepcopy(current_diagonal))
+        print('Appended.')
+    return leftdiagonals
 
 twentygrid = open('twentygrid.txt')
-row_list = build_rows(twentygrid)
+#row_list = build_rows(twentygrid)
 #print(row_list)
-column_list = build_columns(twentygrid)
-print(column_list)
+#column_list = build_columns(twentygrid)
+#print(column_list)
+leftdiagonal_list = build_leftdiagonal(twentygrid)
+print(leftdiagonal_list)
