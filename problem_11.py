c37d5d5d70954706da3ed982cb048c7569e5f913
mzeng7 <mzeng7@berkeley.edu>
2017-12-05 15:33:00 -0800

    complete problem 11

--- problem_11.py
+++ problem_11.py
@@ -1,5 +1,3 @@
-#INCOMPLETE
-
 from copy import deepcopy
 
 def build_rows(grid):
@@ -29,7 +27,8 @@ def build_columns(grid):
 def build_diagonal(grid, start):
     """if start is 'left', then start from the top left corner
     and go towards the bottom right. if start is 'right', then
-    start from the top right corner and go towards to bottom left."""
+    start from the top right corner and go towards to bottom left.
+    Returns all diagonals with length >= 4."""
     if start == 'left':
         current_row, current_column = 1, 1
     elif start == 'right':
@@ -45,7 +44,7 @@ def build_diagonal(grid, start):
         while not end:
             grid.seek((60 * (current_row - 1)) + (3 * (current_column - 1)))
             current_diagonal.append(int(grid.read(2)))
-            print(current_diagonal)
+            #print(current_diagonal)
             if start == 'left':
                 current_column -= 1
                 current_row += 1
@@ -56,9 +55,24 @@ def build_diagonal(grid, start):
                 current_row += 1
                 if current_column > 20:
                     end = True
-        diagonals.append(deepcopy(current_diagonal))
-        print('Appended.')
+        if len(current_diagonal) >= 4:
+            diagonals.append(deepcopy(current_diagonal))
+            #print('Appended.')
+        #else:
+            #print('Not appended because length is less than 4.')
+    grid.seek(0)
     return diagonals
+
+def fourproducts_max(current_list, previous_max):
+    current_max = previous_max
+    for l in current_list:
+        for current_pos in range(len(l) - 4):
+            current_product = 1
+            for i in range(4):
+                current_product = current_product * l[i+(current_pos - 1)]
+            if current_product > current_max:
+                current_max = current_product
+    return current_max
 
 twentygrid = open('twentygrid.txt')
 row_list = build_rows(twentygrid)
@@ -66,6 +80,11 @@ row_list = build_rows(twentygrid)
 column_list = build_columns(twentygrid)
 #print(column_list)
 leftdiagonal_list = build_diagonal(twentygrid, 'left')
-print(leftdiagonal_list)
+#print(leftdiagonal_list)
 rightdiagonal_list = build_diagonal(twentygrid, 'right')
-print(rightdiagonal_list)
+#print(rightdiagonal_list)
+lists = [row_list, column_list, leftdiagonal_list, rightdiagonal_list]
+the_max = 0
+for l in range(4):
+    the_max = fourproducts_max(lists[l], the_max)
+print('The maximum product is: {0}'.format(the_max))
