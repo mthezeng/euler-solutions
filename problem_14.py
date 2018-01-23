d618e635aab5f181169d76dd5ce6684adcc6615b
mzeng7 <mzeng7@berkeley.edu>
2018-01-22 19:14:38 -0800

    using floordiv instead of truediv

--- problem_14.py
+++ problem_14.py
@@ -5,7 +5,7 @@ def collatz(num):
     counter = 1
     while num != 1:
         if num % 2 == 0:
-            num = num / 2
+            num = num // 2
         else:
             num = 3 * num + 1
         counter += 1
