319b317446f3723a35b6f5a0a5ec6ea7f77a879c
mzeng7 <mzeng7@berkeley.edu>
2018-01-29 03:00:29 -0800

    cleanup problem 6

--- problem_06.py
+++ problem_06.py
@@ -1,13 +1,4 @@
-import numpy as np
-
-def sum_100squares():
-    return int((100*101*201)/6)
-
-def square_100sum():
-    return int((101*100)/2) ** 2
-
-sum_of_squares = sum_100squares()
-square_of_sum = square_100sum()
-diff = sum_of_squares - square_of_sum
-print('{0} minus {1} equals {2}'.format(sum_of_squares, square_of_sum, diff))
-# use absolute value
+sum_of_squares = (100*101*201)//6
+square_of_sum = ((101*100)//2) ** 2
+diff = abs(sum_of_squares - square_of_sum)
+print('The difference between {0} and {1} is {2}'.format(sum_of_squares, square_of_sum, diff))
