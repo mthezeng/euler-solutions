735daff908f46efd1cdbc147f7c046546f2facba
mzeng7 <mzeng7@berkeley.edu>
2018-01-29 00:08:25 -0800

    add problem 28

---
+++ problem_28.py
@@ -0,0 +1,22 @@
+from timer import timer
+
+def spiral_diagonal_sum(quadratic):
+    s = 0
+    for i in range(1,501):
+        s += quadratic(i)
+    return s
+
+def generate_quadratics():
+    quadratic0 = lambda x: 4*(x*x) - 2*x + 1
+    quadratic1 = lambda x: 4*(x*x) + 2*x + 1
+    quadratic2 = lambda x: 4*(x*x) + 1
+    quadratic3 = lambda x: 4*(x*x) + 4*x + 1
+    return [quadratic0, quadratic1, quadratic2, quadratic3]
+
+def main():
+    overall_sum = 1
+    for q in generate_quadratics():
+        overall_sum += spiral_diagonal_sum(q)
+    print(overall_sum)
+
+timer(main)
