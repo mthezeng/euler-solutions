11184eb391b3976a4c2178b9b0317a34ebd4ef1d
mzeng7 <mzeng7@berkeley.edu>
2018-02-22 20:48:36 -0800

    improved

--- p001.py
+++ p001.py
@@ -1,3 +1,23 @@
+from timer import timer
+
+# Improved #
+
+def sum_of_naturals_upto(n):
+    return (n * (n + 1)) // 2
+
+def improved(n):
+    fives = 5 * sum_of_naturals_upto((n - 1) // 5)
+    threes = 3 * sum_of_naturals_upto((n - 1) // 3)
+    fifteens = 15 * sum_of_naturals_upto((n - 1) // 15)
+    return fives + threes - fifteens
+
+def main0():
+    print(improved(1000))
+
+timer(main0)
+
+# Legacy #
+
 def three_five_multiples(n):
     s = 0
     for i in range(n):
@@ -5,4 +25,7 @@ def three_five_multiples(n):
             s = s + i
     return s
 
-print(three_five_multiples(1000))
+def main1():
+    print(three_five_multiples(1000))
+
+timer(main1)
